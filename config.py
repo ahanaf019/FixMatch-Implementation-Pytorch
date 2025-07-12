e6b40994231e698509281729e994fdaf4e3bee16
Md. Ahanaf Arif Khan <ahanaf019@gmail.com>
2025-07-12 14:00:26 +0600

    Fix minor issues in implementation

--- config.py
+++ config.py
@@ -1,11 +1,54 @@
 import os
+from torchvision import transforms
 
 DB_PATH_ROOT = f'{os.environ["HOME"]}/Datasets/STL10'
 BATCH_SIZE = 128
 IMAGE_SIZE = 96
+WARMUP_EPOCHS = 5
 LEARNING_RATE = 1e-4
 NUM_EPOCHS = 20
 
 NUM_CLASSES = 10
 NUM_UNLABELED = 15000
 THRESHOLD = 0.90
+LAMBDA_U = 1.0
+
+
+weak_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomResizedCrop(size=(IMAGE_SIZE, IMAGE_SIZE), scale=(0.8, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean =[0.4914 , 0.4822 , 0.4465] , 
+        std=[0.2470 , 0.2435 , 0.2616]
+    ),
+])
+
+
+strong_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomResizedCrop(size=(IMAGE_SIZE, IMAGE_SIZE), scale=(0.8, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.ColorJitter(
+        brightness=0.4,
+        contrast=0.4,
+        saturation=0.4,
+        hue=0.1
+    ),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean =[0.4914 , 0.4822 , 0.4465] , 
+        std=[0.2470 , 0.2435 , 0.2616]
+    ),
+])
+
+
+val_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean =[0.4914 , 0.4822 , 0.4465] , 
+        std=[0.2470 , 0.2435 , 0.2616]
+    ),
+])
--- main.py
+++ main.py
@@ -4,10 +4,11 @@ os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 from glob import glob
 import shutil
 from tqdm.auto import tqdm
-
+from itertools import cycle
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import numpy as np
@@ -20,46 +21,6 @@ from datasets.semisupervised_dataset import SSDataset
 from models.cnn_model import CNNModel
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-
-weak_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomResizedCrop(size=(IMAGE_SIZE, IMAGE_SIZE), scale=(0.8, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean =[0.4914 , 0.4822 , 0.4465] , 
-        std=[0.2470 , 0.2435 , 0.2616]
-    ),
-])
-
-
-strong_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomResizedCrop(size=(IMAGE_SIZE, IMAGE_SIZE), scale=(0.8, 1.0), interpolation=transforms.InterpolationMode.BICUBIC),
-    transforms.ColorJitter(
-        brightness=0.4,
-        contrast=0.4,
-        saturation=0.4,
-        hue=0.1
-    ),
-    transforms.RandomGrayscale(p=0.2),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean =[0.4914 , 0.4822 , 0.4465] , 
-        std=[0.2470 , 0.2435 , 0.2616]
-    ),
-])
-
-
-val_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean =[0.4914 , 0.4822 , 0.4465] , 
-        std=[0.2470 , 0.2435 , 0.2616]
-    ),
-])
 
 
 def main():
@@ -96,19 +57,28 @@ def main():
     unlabeled_loader = DataLoader(unlabeled_db, batch_size=BATCH_SIZE, shuffle=True, num_workers=os.cpu_count())
 
 
-    model = CNNModel(num_classes=NUM_CLASSES).to(device)
+    model = CNNModel(num_classes=NUM_CLASSES, dropout_rate=0.25).to(device)
     summary(model, input_size=[1, 3, 96, 96])
 
     optim = torch.optim.AdamW(model.parameters(), lr=1e-4)
     loss_fn = nn.CrossEntropyLoss()
     acc_metric = MulticlassAccuracy(num_classes=NUM_CLASSES).to(device)
-
+    scheduler = torch.optim.lr_scheduler.SequentialLR(
+        optimizer=optim, schedulers=[
+            torch.optim.lr_scheduler.LinearLR(
+                optim, start_factor=0.001, end_factor=1, total_iters=WARMUP_EPOCHS 
+            ),
+            torch.optim.lr_scheduler.CosineAnnealingLR(
+                optim, T_max=NUM_EPOCHS - WARMUP_EPOCHS
+            )
+        ], milestones=[WARMUP_EPOCHS])
+    
     for epoch in range(NUM_EPOCHS):
         model.train()
         losses = []
         val_losses = []
 
-        for (labeled_images, labels), (weak_img, strong_img) in tqdm(zip(train_loader, unlabeled_loader)):
+        for (labeled_images, labels), (weak_img, strong_img) in tqdm(zip(cycle(train_loader), unlabeled_loader)):
             labeled_images, labels = labeled_images.to(device), labels.to(device)
 
             weak_img, strong_img = weak_img.to(device), strong_img.to(device)
@@ -117,22 +87,24 @@ def main():
             labelled_preds = model(labeled_images)
             loss_labelled = loss_fn(labelled_preds, labels)
 
-            with torch.no_grad():
-                weak_preds = model(weak_img)
-                pseudo_labels = torch.softmax(weak_preds, dim=1)
-                max_probs, targets_u = pseudo_labels.max(dim=1)
+            # with torch.no_grad():
+            #     weak_preds = model(weak_img)
+            #     pseudo_labels = torch.softmax(weak_preds, dim=1)
+            #     max_probs, targets_u = pseudo_labels.max(dim=1)
             
-            mask = max_probs > THRESHOLD
+            # mask = max_probs > THRESHOLD
 
-            strong_preds = model(strong_img)
-            loss_unlabeled = (loss_fn(strong_preds, targets_u) * mask).mean()
+            # strong_preds = model(strong_img)
+            # loss_unlabeled_all = F.cross_entropy(strong_preds, targets_u, reduction='none')
+            # loss_unlabeled = (loss_unlabeled_all* mask).mean()
 
-            loss = loss_labelled + loss_unlabeled
+            loss = loss_labelled #+ LAMBDA_U * loss_unlabeled
             losses.append(loss.item())
 
             optim.zero_grad()
             loss.backward()
             optim.step()
+
 
         acc_metric.reset()
         model.eval()
@@ -145,8 +117,8 @@ def main():
                 loss_labelled = loss_fn(labelled_preds, labels)
 
             acc_metric.update(labelled_preds, labels)
-            val_losses.append(loss.item())
-        
+            val_losses.append(loss_labelled.item())
+        scheduler.step()
         print(f" Epoch [{ epoch +1}/{ NUM_EPOCHS }] , Loss : {np.mean(losses):0.4f} | val_loss: {np.mean(val_losses):0.4f} | Accuracy: {acc_metric.compute().cpu().item():0.4f}")
 
 
